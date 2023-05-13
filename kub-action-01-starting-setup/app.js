c2505dcb6c6649c8c3af1d0d5ef3d784b2173761
Athirson <athirsonarceus@gmail.com>
2023-05-12 21:34:11 -0300

    kubernetes fundamentals and core concepts

--- kub-action-01-starting-setup/app.js
+++ kub-action-01-starting-setup/app.js
@@ -4,10 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => {
 	res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this NodeJS app!!!!!!!</h1>
     <p>Try sending a request to /error and see what happens</p>
 		<p>Try sending a request to /version and see what happens</p>
-		<small>Version 2.1.0</small>
+		<img src="https://giphy.com/gifs/minions-minions-2-rise-of-gru-StKiS6x698JAl9d6cx" />
+		<small>Version 2.2.0</small>
   `);
 });
 
